10628935ce8e30c81ca469d36218c10cb32a05c6
TokisakiKurumi2001 <minhkhoile2001nsg@gmail.com>
2022-12-31 20:54:21 +0700

    Training with longer sequence

--- train.py
+++ train.py
@@ -14,13 +14,13 @@ if __name__ == "__main__":
     lit_phrext = LitPhrExt(**hyperparameter)
 
     # dataloader
-    phrext_dataloader = PhrExtDataLoader(pretrained_ck='roberta-base', max_length=25)
-    [train_dataloader, test_dataloader, valid_dataloader] = phrext_dataloader.get_dataloader(batch_size=128, types=["train", "test", "validation"])
+    phrext_dataloader = PhrExtDataLoader(pretrained_ck='roberta-base', max_length=180)
+    [train_dataloader, test_dataloader, valid_dataloader] = phrext_dataloader.get_dataloader(batch_size=64, types=["train", "test", "validation"])
 
     # train model
-    trainer = pl.Trainer(max_epochs=20, devices=[1], accelerator="gpu", logger=wandb_logger)#, strategy="ddp")
+    trainer = pl.Trainer(max_epochs=25, devices=[0], accelerator="gpu", logger=wandb_logger)#, strategy="ddp")
     trainer.fit(model=lit_phrext, train_dataloaders=train_dataloader, val_dataloaders=valid_dataloader)
     trainer.test(dataloaders=test_dataloader)
 
     # save model & tokenizer
-    lit_phrext.export_model('phrext_model/v1')
+    lit_phrext.export_model('phrext_model/v2')
